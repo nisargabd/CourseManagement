3dcc35ca1e3f602b18f7733f87265c79e7446cc2
Nisarga B.D <nisar@Nisarga.com>
2025-10-23 17:48:51 +0530

    added .gitignore

--- src/main/java/com/sanketika/course_backend/CourseBackendApplication.java
+++ src/main/java/com/sanketika/course_backend/CourseBackendApplication.java
@@ -1,5 +1,6 @@
 package com.sanketika.course_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +10,11 @@ import org.springframework.cache.annotation.EnableCaching;
 public class CourseBackendApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(CourseBackendApplication.class, args);
 	}
 
